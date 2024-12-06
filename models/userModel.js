34c44c0f17216cc8defe48c7a847cdfdf8861e13
rifal12330 <rifalmuhamad12330@gmail.com>
2024-12-06 18:20:32 +0700

    Pesan commit

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const db = require('../config/db');  // Import koneksi database dari db.js
 // Fungsi untuk membuat pengguna baru
 const createUser = (name, email, password, callback) => {
   // Menyisipkan pengguna baru ke dalam tabel users
-  const query = 'INSERT INTO users (name, email, password, created_at, updated_at) VALUES (?, ?, ?, NOW(), NOW())';
+  const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
   db.query(query, [name, email, password], (err, result) => {
     if (err) {
       console.error('Error inserting user:', err);
@@ -13,9 +13,9 @@ const createUser = (name, email, password, callback) => {
     // Mengecek jika email berdomain @lawyer.com, maka insert ke tabel lawyers
     if (email.endsWith('@lawyer.com')) {
       const userId = result.insertId;  // Mengambil user_id yang baru dimasukkan
-      const lawyerQuery = 'INSERT INTO lawyers (user_id, name, specialization, ktpa, ratings, experience_years, contact, availability, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())';
+      const lawyerQuery = 'INSERT INTO lawyers (user_id, name, specialization, ktpa, ratings, experience_years, contact, availability) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
       const lawyerData = [userId, name, 'Unknown', 'Unknown', 0, 0, email, true];  // Data default untuk lawyer
-
+      
       db.query(lawyerQuery, lawyerData, (err, lawyerResult) => {
         if (err) {
           console.error('Error inserting into lawyers:', err);
